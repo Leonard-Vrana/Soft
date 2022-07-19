af3cb0a233d1049b73051b83cd3bb8ef00951e91
suhajisko <94246799+Leonard-Vrana@users.noreply.github.com>
2022-07-19 20:04:13 +0200

    Add auth middleware

--- routes/user.php
+++ routes/user.php
@@ -2,15 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\UserController@homepage')->name('homepage')
-                                                                     ->middleware('auth');
+Route::middleware('auth')->group(function () {
+    Route::get('/', 'App\Http\Controllers\UserController@homepage')->name('homepage');
 
-Route::post("/", "App\Http\Controllers\CarController@createCar")->name("createCar");
-Route::post("/edit-car", "App\Http\Controllers\CarController@editCar")->name("editCar");
-Route::post("/delete-car", "App\Http\Controllers\CarController@deleteCar")->name("deleteCar");
+    Route::post("/", "App\Http\Controllers\CarController@createCar")->name("createCar");
+    Route::post("/edit-car", "App\Http\Controllers\CarController@editCar")->name("editCar");
+    Route::post("/delete-car", "App\Http\Controllers\CarController@deleteCar")->name("deleteCar");
 
 
-Route::resource('/detail', 'App\Http\Controllers\PartController')->only('show');
-Route::post('/detail/create', 'App\Http\Controllers\PartController@create')->name('partCreate');
-Route::post('/detail/edit', 'App\Http\Controllers\PartController@edit')->name('partEdit');
-Route::post('/detail/delete', 'App\Http\Controllers\PartController@delete')->name('partDelete');
+    Route::resource('/detail', 'App\Http\Controllers\PartController')->only('show');
+    Route::post('/detail/create', 'App\Http\Controllers\PartController@create')->name('partCreate');
+    Route::post('/detail/edit', 'App\Http\Controllers\PartController@edit')->name('partEdit');
+    Route::post('/detail/delete', 'App\Http\Controllers\PartController@delete')->name('partDelete');
+});
